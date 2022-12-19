efb3fbd3da9eb6362ed83b41692b6ab894b184b5
Nibbanovic <120715125+Nibbanovic@users.noreply.github.com>
2022-12-19 12:45:11 +0100

    added exercise 11 and 12

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+
+print(f"So you're {age} years old and {height} centimeters tall.")
+
+# -----
+
